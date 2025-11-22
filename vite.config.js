8e5f8afa8b706181137c235af991a7822b22b50b
MhJibril <128263683+MhJibril@users.noreply.github.com>
2025-11-22 20:09:19 +0700

    Change base path in vite.config.js
    Update base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: "./",
-});
+  base: '/sonic-runner/',  // <- penting untuk GitHub Pages
+})
